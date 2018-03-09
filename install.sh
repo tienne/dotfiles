f58112b7a188dd1c8dea70e519f6e822e1ff30d6
tienne <tienne89@gmail.com>
2018-03-09 18:52:41 +0900

    kubectl 추가

--- install.sh
+++ install.sh
@@ -146,6 +146,13 @@ icask docker
 icask docker-toolbox
 
 ###
+bot "kubernetes"
+###
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/darwin/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+###
 bot "ide and dev tool"
 ###
 icask sourcetree
